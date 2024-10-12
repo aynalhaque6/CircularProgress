4638ae9ae7ea2130fea3d86a96789f609b98fa6c
AHM SOFT PC <aynalhaqueimon@gmail.com>
2024-10-12 20:31:42 +0600

    Third Commit

--- app/src/main/java/com/codecollection/circularprogressbutton/MainActivity.java
+++ app/src/main/java/com/codecollection/circularprogressbutton/MainActivity.java
@@ -1,8 +1,11 @@
 package com.codecollection.circularprogressbutton;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.codecollection.circularprogress.ProgressButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,10 +13,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ProgressButton progressButton = findViewById(R.id.sendingBtn);
+        progressButton.setOnProgressCompleteListener(() -> Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show());
+        
     }
 }
